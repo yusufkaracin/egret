acb8c90ac8fd8fb5133c7ce2bce25d9055f28d5f
Salih Can Düşmez <salihcandusmez@Salih-MacBook-Pro.local>
2016-04-23 16:07:53 +0300

    iskelet oluştu

--- app/scripts/angularfire/auth.js
+++ app/scripts/angularfire/auth.js
@@ -1,7 +1,6 @@
 (function() {
   'use strict';
   angular.module('firebase.auth', ['firebase', 'firebase.ref'])
-
     .factory('Auth', function($firebaseAuth, Ref) {
       return $firebaseAuth(Ref);
     });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,9 @@
  * @name salihcandusmezApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the salihcandusmezApp
+ *
  */
 angular.module('salihcandusmezApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.deneme = 'lorem ipsum';
   });
