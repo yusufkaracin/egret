39cc1660823b3e40704241e1dbb5594d55970a15
Salih Can Düşmez <salihcandusmez@Salih-MacBook-Pro.local>
2016-04-23 15:29:57 +0300

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name salihcandusmezApp
+ * @description
+ * # salihcandusmezApp
+ *
+ * Main module of the application.
+ */
+angular.module('salihcandusmezApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngRoute',
+    'ngSanitize',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name salihcandusmezApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the salihcandusmezApp
+ */
+angular.module('salihcandusmezApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
